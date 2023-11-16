fb0d18c7a55a95fb10044fb5ee268ac0552fce38
omodev <omooladev@gmail.com>
2023-11-16 14:50:03 +0100

    worked on the next button

--- public/js/shop/shop.js
+++ public/js/shop/shop.js
@@ -1,4 +1,4 @@
-const productImages = document.querySelectorAll(".shop-product-images");
+const productImages = document.querySelectorAll(".product-image-container .product-images");
 const prevArrow = document.querySelectorAll(".prev-arrow");
 const nextArrow = document.querySelectorAll(".next-arrow");
 
@@ -8,18 +8,31 @@ const hoverImageHandler = (event) => {
   if (imageUrls.length === 1) {
     return;
   }
+  console.log("show arrow");
 };
-const viewNextImageHandler = (event) => {let actualImage;
+const viewNextImageHandler = (event) => {
   for (let i = 0; i < productImages.length; i++) {
     if (productImages[i].dataset.imageId === event.target.dataset.imageId) {
-      return productImages[i];
+      const imageIndexNumber = Number(productImages[i].dataset.imageIndexNumber) + 1;
+
+      if (imageIndexNumber === productImages.length - 1) {
+        nextArrow[i].disabled = true;
+        prevArrow[i].disabled = false;
+      }
+      if (0 < productImages[i].dataset.imageIndexNumber < productImages.length) {
+        nextArrow[i].disabled = false;
+        prevArrow[i].disabled = false;
+
+        productImages[i].dataset.imageIndexNumber = imageIndexNumber;
+        productImages[i].children[imageIndexNumber - 1].width = "0%";
+        productImages[i].children[imageIndexNumber].width = "100%";
+      }
       break;
     }
   }
-  console.log(actualImage);
 };
-productImages.forEach((image) => {
-  image.addEventListener("mouseover", hoverImageHandler);
+productImages.forEach((productImageContainer) => {
+  productImageContainer.addEventListener("mouseover", hoverImageHandler);
 });
 nextArrow.forEach((nextArrow) => {
   nextArrow.addEventListener("click", viewNextImageHandler);
