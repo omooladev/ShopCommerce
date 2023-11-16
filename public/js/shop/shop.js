b668c0cf52c01b9d3b9b6dbe19fe3bbbf8aaeea8
omodev <omooladev@gmail.com>
2023-11-16 23:06:28 +0100

    finished the view previous image handler and view next image handler

--- public/js/shop/shop.js
+++ public/js/shop/shop.js
@@ -11,29 +11,80 @@ const hoverImageHandler = (event) => {
   console.log("show arrow");
 };
 const viewNextImageHandler = (event) => {
-  for (let i = 0; i < productImages.length; i++) {
-    if (productImages[i].dataset.imageId === event.target.dataset.imageId) {
-      const imageIndexNumber = Number(productImages[i].dataset.imageIndexNumber) + 1;
+  event.stopPropagation();
+  let targetElement = event.target;
+  if (event.target.className.includes("icon")) {
+    targetElement = event.target.parentElement;
+  }
 
-      if (imageIndexNumber === productImages.length - 1) {
-        nextArrow[i].disabled = true;
-        prevArrow[i].disabled = false;
-      }
-      if (0 < productImages[i].dataset.imageIndexNumber < productImages.length) {
-        nextArrow[i].disabled = false;
-        prevArrow[i].disabled = false;
+  const nextArrow = targetElement;
 
-        productImages[i].dataset.imageIndexNumber = imageIndexNumber;
-        productImages[i].children[imageIndexNumber - 1].width = "0%";
-        productImages[i].children[imageIndexNumber].width = "100%";
-      }
-      break;
+  const prevArrow = targetElement.parentElement.children[0];
+  const productImagesElement = targetElement.parentElement.parentElement.children[0];
+  const productImagesArray = targetElement.parentElement.parentElement.children[0].children;
+
+  //----------> check if the length of the images is only one
+  if (productImagesArray.length === 1) {
+    return;
+  }
+  let imageIndexNumber = Number(productImagesElement.dataset.imageIndexNumber);
+  if (imageIndexNumber < productImagesArray.length - 1) {
+    imageIndexNumber += 1;
+    console.log(imageIndexNumber);
+    productImagesElement.dataset.imageIndexNumber = imageIndexNumber;
+    productImagesArray[imageIndexNumber - 1].classList.remove("active");
+    productImagesArray[imageIndexNumber].classList.add("active");
+    //----------> check if the previous arrow is already disabled
+    if (prevArrow.disabled) {
+      prevArrow.disabled = false;
+    }
+    if (imageIndexNumber === productImagesArray.length - 1) {
+      nextArrow.disabled = true;
     }
   }
 };
-productImages.forEach((productImageContainer) => {
-  productImageContainer.addEventListener("mouseover", hoverImageHandler);
+const viewPreviousImageHandler = (event) => {
+  event.stopPropagation();
+  let targetElement = event.target;
+  if (event.target.className.includes("icon")) {
+    targetElement = event.target.parentElement;
+  }
+  //----------> get all the elements needed
+  const prevArrow = targetElement;
+  const nextArrow = targetElement.parentElement.children[1];
+  const productImagesElement = targetElement.parentElement.parentElement.children[0];
+  const productImagesArray = targetElement.parentElement.parentElement.children[0].children;
+
+  //----------> If the length of images in one, do nothing
+  if (productImagesArray.length === 1) {
+    return;
+  }
+  let imageIndexNumber = Number(productImagesElement.dataset.imageIndexNumber);
+
+  // if (imageIndexNumber === 0) {
+  //   return;
+  // }
+  if (imageIndexNumber > 0) {
+    imageIndexNumber -= 1;
+    productImagesElement.dataset.imageIndexNumber = imageIndexNumber;
+    productImagesArray[imageIndexNumber + 1].classList.remove("active");
+    productImagesArray[imageIndexNumber].classList.add("active");
+    //----------> check if the next arrow is already disabled
+
+    if (nextArrow.disabled) {
+      nextArrow.disabled = false;
+    }
+    if (imageIndexNumber === 0) {
+      prevArrow.disabled = true;
+    }
+  }
+};
+productImages.forEach((productImages) => {
+  productImages.addEventListener("mouseover", hoverImageHandler);
 });
 nextArrow.forEach((nextArrow) => {
   nextArrow.addEventListener("click", viewNextImageHandler);
+});
+prevArrow.forEach((prevArrow) => {
+  prevArrow.addEventListener("click", viewPreviousImageHandler);
 });
