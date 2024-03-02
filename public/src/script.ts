a0c25b9070baa88c6c3f8d66bfb2010655d4bdde
Omosuyi Olawole <omooladev@gmail.com>
2024-03-02 14:48:16 +0100

    extended the typescript configuration of the client to the root typescript configuration

--- public/src/script.ts
+++ public/src/script.ts
@@ -1,5 +1,5 @@
-"use strict";
 // const productDescription = document.querySelectorAll(".product_description");
+
 // productDescription.forEach((description) => {
 //   if (description.innerHTML.trim().length > 220) {
 //     description.innerHTML = description.innerHTML.substring(0, 220) + "...";
