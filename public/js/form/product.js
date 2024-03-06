87cb0b22bd01f80b74830bc294befad3890f3ca4
Omosuyi Olawole <omooladev@gmail.com>
2024-03-06 12:05:59 +0100

    working on the form submission

--- public/js/form/product.js
+++ public/js/form/product.js
@@ -1,8 +1,10 @@
 //<---------- Import modules ---------->
 import { changeProductInputHandler } from "../functions/ProductInputHandler.js";
+import { submitFormHandler } from "../functions/formHandler.js";
 import { changeImageHandler } from "../functions/imageHandler.js";
 //<---------- EVENT LISTENERS FOR PRODUCTS INPUTS ---------->
 productName.oninput = (event) => changeProductInputHandler(event, "Name");
 productPrice.oninput = (event) => changeProductInputHandler(event, "Price");
 productDescription.oninput = (event) => changeProductInputHandler(event, "Description");
 productImage.onchange = (event) => changeImageHandler(event, "Image");
+productForm.onsubmit = (event) => submitFormHandler(event);
--- public/src/form/product.ts
+++ public/src/form/product.ts
@@ -1,9 +1,11 @@
 //<---------- Import modules ---------->
 import { changeProductInputHandler } from "../functions/ProductInputHandler.js";
+import { submitFormHandler } from "../functions/formHandler.js";
 import { changeImageHandler } from "../functions/imageHandler.js";
 
 //<---------- EVENT LISTENERS FOR PRODUCTS INPUTS ---------->
 productName.oninput = (event: Event) => changeProductInputHandler(event, "Name");
 productPrice.oninput = (event: Event) => changeProductInputHandler(event, "Price");
 productDescription.oninput = (event: Event) => changeProductInputHandler(event, "Description");
-productImage.onchange = (event: Event) => changeImageHandler(event,"Image");
+productImage.onchange = (event: Event) => changeImageHandler(event, "Image");
+productForm.onsubmit = (event: Event) => submitFormHandler(event);
