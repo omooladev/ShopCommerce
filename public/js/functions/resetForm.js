e160cfd910ca74972490ba7e7b33bb86047f7452
Omosuyi Olawole <omooladev@gmail.com>
2024-03-11 16:21:19 +0100

    finished resetting form fields

--- public/js/functions/resetForm.js
+++ public/js/functions/resetForm.js
@@ -2,21 +2,31 @@ import { toggleFilePickerDisabledState } from "../utils/TransformImage.js";
 import { saveFormValidity } from "./saveFormValidity.js";
 import { setFormReply } from "./setFormReply.js";
 const resetPreviewImageContainer = () => {
+    //----------> loop through and delete the preview images found in the preview image
+    //            container
     for (let index = previewImageContainer.children.length - 1; index >= 0; index--) {
+        //----------> the child element
         let child = previewImageContainer.children[index];
+        //----------> remove the child
         previewImageContainer.removeChild(child);
-        console.log("removed");
     }
+    //----------> get the primary class
+    const defaultClassName = previewImageContainer.classList[0];
     //----------> reset the class
-    previewImageContainer.className = "preview_image_container";
+    previewImageContainer.className = defaultClassName;
 };
 export const resetForm = () => {
     resetPreviewImageContainer();
     productImageFiles = [];
     transformedImages = [];
+    //----------> reset the disabled state of the file input
     toggleFilePickerDisabledState(productImageFiles);
+    //----------> reset the form
     productForm.reset();
+    //----------> configure the length to default
     productDescriptionLength.innerHTML = "0";
+    //----------> save the form validity
     saveFormValidity("form-reset");
+    //---------->reset the form reply
     setFormReply("", "", "reset");
 };
